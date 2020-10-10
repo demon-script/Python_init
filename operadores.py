61e1d6dff6d920cad21c93640966c85eadbd30ea
Demonscript <demonscript.io@gmail.com>
2020-10-10 00:11:44 -0500

    version 0.0.3
    implementación inicial de variables y operadores

---
+++ operadores.py
@@ -0,0 +1,10 @@
+#vamos a implementar el uso de operadores Básicos..
+
+#el módulo de una división...
+print(7 % 3)
+
+#división entera...
+print(7 // 3)
+
+#operador de exponente...
+print(7 ** 3)
---
+++ variables.py
@@ -0,0 +1,16 @@
+#Variables y concatenación..
+text1 = "Hello Demonscript "
+text2 = "Estas aprendiendo Python"
+union_text = text1 + text2
+print(union_text)
+
+#otra manera de concatenar es en el moento de imprimir..
+print(text1 + text2)
+#concatenar cadenas con format...
+nombre = 'J2logo'
+f'Hola {nombre}'
+print(f'el n')
+# Variables y conversión a texto...
+num1 = 39
+print(num1)
+print("El número "+str(num1)+" es de tipo "+str(type(num1)))
